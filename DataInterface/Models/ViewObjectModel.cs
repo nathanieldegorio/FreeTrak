974654fef3c64e341dfd50311cbd1cd1932073c8
Nathaniel Degorio <nbdegorio@gmail.com>
2017-11-04 10:58:34 +0800

    initial commit

---
+++ DataInterface/Models/ViewObjectModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface.Models
+{
+    public class ViewObjectModel
+    {
+        string key { get; set; }
+        object value { get; set; }
+    }
+}
