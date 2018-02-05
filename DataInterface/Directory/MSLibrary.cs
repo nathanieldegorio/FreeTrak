3fc16326889eb87e06dcd9f0a2a0bc3bd65b7eea
Nathaniel Degorio <nbdegorio@gmail.com>
2018-02-05 17:55:22 +0800

    -commit new kiosk

---
+++ DataInterface/Directory/MSLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface.Directory
+{
+    public class MSLibrary
+    {
+        public static string Login = "Select UserID, UserDesignation from userAccounts where login=@login and pass=@pass";
+        public static string getMenuAccess = "SELECT menuID, userDesignationAccess , defaultDashboard FROM dbo.tbl_designationMenus where userDesignation = @userDesignation";
+
+    }
+}
