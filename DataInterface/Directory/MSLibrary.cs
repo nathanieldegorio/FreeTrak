d4f3973cd713d99c8ab17fb75b13b632d2985f8a
Nathaniel Degorio <nbdegorio@gmail.com>
2018-02-17 15:31:22 +0800

    -commit new UI

--- DataInterface/Directory/MSLibrary.cs
+++ DataInterface/Directory/MSLibrary.cs
@@ -6,8 +6,7 @@ namespace DataInterface.Directory
 {
     public class MSLibrary
     {
-        public static string Login = "Select UserID, UserDesignation from userAccounts where login=@login and pass=@pass";
-        public static string getMenuAccess = "SELECT menuID, userDesignationAccess , defaultDashboard FROM dbo.tbl_designationMenus where userDesignation = @userDesignation";
+       
 
     }
 }
